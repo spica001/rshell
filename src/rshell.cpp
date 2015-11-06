3f05cc0dd9cc81ff2de9aca3a0e191f92cd35821
spica001 <spica001@ucr.edu>
2015-11-06 13:26:57 -0800

    update for extra credit

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-#include <string>
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <sys/types.h>
-#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <vector>
+#include <string>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
 
 //FIRST PUSH - still buggy etc
 
@@ -22,7 +22,7 @@ int min2(int a, int b)
   {
     return a;
   }
-  
+
   if(a < b)
   {
     return a;
@@ -38,24 +38,24 @@ int min3(int a, int b, int c)
   {
     return min2(b,c);
   }
-  
+
   if(b < 0)
   {
     return min2(a,c);
   }
-  
+
   if(c < 0)
   {
     return min2(a,b);
   }
-  
+
   if(a < b)
   {
     if(a < c)
     {
       return a;
     }
-    else 
+    else
     return c;
   }
   else if(b < c)
@@ -72,7 +72,7 @@ int main()
 {
  // ; , || and &&
  // look for each of them to check
- 
+
   string input;
   string com; //individual commands from the vector
   int semicolon = 0;
@@ -83,62 +83,71 @@ int main()
   int otherStart = 0; // stores the old value of start.
   vector<string> commands; //vector to store the commands inside.
   const vector<char*> execcommands;
-  
-  
+
+
   while(true) // want infinite loop until break
   {
-    
-	  cout <<"$ ";
-	  getline (cin, input);
-     
+     if (getlogin() != NULL)
+    {
+		cout << getlogin() << "@";
+    }
+    char servername[64] = {0};
+    if (gethostname(servername, 64) != -1)
+    {
+		gethostname(servername, 64);
+		cout << servername << " ";
+    }
+    cout <<"$ ";
+	getline (cin, input);
+
     sharp = input.find("#", start);
     input = input.substr(0, sharp); //this gets everything from 0 to before sharp - so this is the input?
-  
-    
-     
- 
+
+
+
+
    while(true)
    {
       //these finds are used to get the commands
       semicolon = input.find(";",start);
       bar = input.find("||",start);
       ands = input.find("&&",start);
-      
-  
+
+
       //cout << min3(semicolon,bar,ands) << endl; - testing purposes
-    
+
       start = min3(semicolon,bar,ands)+1;
-      
+
       if(input[start] == '#' || input[otherStart] == '#')
       {
         break;
       }
-      
-      
-      
+
+
+
       if(input[otherStart] == '|' || input[otherStart] == '&')
       {
         otherStart+=1; //if the current start has a non-semicolon
       }
       com = input.substr(otherStart,(start-otherStart-1)); //start-1 because it would include the separators
       commands.push_back(com);
-      
-    
+
+
       otherStart = start; //otherstart now becomes the position of Start, which is used for the next iteration
-      
+
       if(start == 0)
       {
         break;
       }
    }
-      
+
    for(int i = 0; i != commands.size(); i++)
    {
      cout << commands.at(i) << endl;
    }
-   
+
   //syscall stuff
-  
+
   for(int j = 0; j != commands.size() ; j++)
   {
     char* temp = const_cast <char*> (commands.at(j).c_str());
@@ -147,7 +156,7 @@ int main()
     int status;
 
     c_pid = fork();
-  
+
     if( c_pid < 0)
     {
       perror("fork failed");
@@ -156,11 +165,11 @@ int main()
 
     else if (c_pid == 0)
     {
-      printf("Child: executing j\n");            
-      execvp(args[0], args);                                             
+      printf("Child: executing j\n");
+      execvp(args[0], args);
       perror("execvp failed");
     }
-    
+
     else if (c_pid > 0)
     {
       if((pid = wait(&status)) < 0)
@@ -172,7 +181,7 @@ int main()
       printf("Parent: finished\n");
 
     }
-    
+
    }
 }
     //ls; mkdir directory; cd directory; touch myfile.txt; ls
